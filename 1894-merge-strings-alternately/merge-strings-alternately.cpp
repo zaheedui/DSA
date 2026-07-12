0f4c5d43859870e615ea09d235c9171d84f4aee6
Syed Zaheed <syedzaheed012@gmail.com>
2026-07-12 23:21:30 +0530

    Time: 6 ms (2.69%) | Memory: 9.4 MB (5.24%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+    int i=0;
+    int j=0;
+    string ans="";
+
+    while(i<word1.length() || j<word2.length()){
+
+        if(i<word1.length()){
+           ans=ans+word1[i];
+            i++;
+        }
+        if(j<word2.length()){
+            ans=ans+word2[j];
+            j++;
+        }
+    }
+    return ans;
+    }
+};
