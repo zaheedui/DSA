9ba8a0ff1efd9613cb795bc03d349234056ea5b5
Syed Zaheed <syedzaheed012@gmail.com>
2026-08-29 10:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.57%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.length()-1;
+        int ans=0;
+
+        
+        while(i>=0 && s[i]==' '){
+        i--;
+        }
+        
+
+        
+
+        while(i>=0 && s[i]!=' '){
+            ans++;
+            i--;
+        }
+
+        return ans;
+    }
+};
