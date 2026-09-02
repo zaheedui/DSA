cbe5c910b414e05ce5939e2703ecd3b07388636d
Syed Zaheed <syedzaheed012@gmail.com>
2026-09-02 18:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (70.07%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-//       int left=0;
-//       int zerocount=0;
-//       int maxLength=0;
+      int left=0;
+      int zerocount=0;
+      int maxLength=0;
       
-//       for(int i=0;i<nums.size();i++){
-//         if(nums[i]==0){
-//             zerocount++;
-//         }
-//         while (zerocount > k){
-//             if(nums[left]==0){
-//                 zerocount--;
-//             }
-//             left++;
-//         }
-//         maxLength=max(maxLength,i-left+1);
-//       }
-// return maxLength;
+      for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+            zerocount++;
+        }
+        while (zerocount > k){
+            if(nums[left]==0){
+                zerocount--;
+            }
+            left++;
+        }
+        maxLength=max(maxLength,i-left+1);
+      }
+return maxLength;
 
-int i=0; 
-        int j = 0;
+// int i=0; 
+//         int j = 0;
         
        
-        int n = nums.size();
-        while (j < n)
-        {
-            if(nums[j] == 0)
-            {
-                k--;
-            }
-            if (k < 0)
-            {
-                if (nums[i] == 0)
-                    k++;
-                i++;
-            }
-            j++;
+//         int n = nums.size();
+//         while (j < n)
+//         {
+//             if(nums[j] == 0)
+//             {
+//                 k--;
+//             }
+//             if (k < 0)
+//             {
+//                 if (nums[i] == 0)
+//                     k++;
+//                 i++;
+//             }
+//             j++;
             
-        }
+//         }
 
-        return j-i;
+//         return j-i;
     }
 };
